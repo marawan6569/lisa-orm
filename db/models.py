f8aa2b2d6be481b825cfec1514a620205300cc2b
marawan6569 <marawan6569@gmail.com>
2021-11-13 22:24:48 +0200

    restart with metaclass

--- db/models.py
+++ db/models.py
@@ -2,60 +2,51 @@ import os
 import sqlite3, traceback, json
 from lisa_settings import PATH
 
-from django.db import migrations
-class Model:
+fields = [
+    {'CharField': 'A field to store text based values.'},
+    {'IntegerField': 'It is an integer field.'},
+    {'BooleanField', 'A true/false field.'},
+    {'AutoField': 'It An IntegerField that automatically increments.'},
+    {'FloatField': 'It is a floating-point number represented in Python by a float instance.'},
+    {'TextField	': 'A large text field. The default form widget for this field is a Textarea.'},
+    {'DateField': 'A date, represented in Python by a datetime.date instance'},
+    {'TimeField	': 'A time, represented in Python by a datetime.time instance.'},
+    {'DateTimeField': 'date and field'},
+    {'ForeignKey': 'A many-to-one relationship. Requires two positional arguments: the class to which the model is related and the on_delete option.'},
+    {'ManyToManyField': 'A many-to-many relationship. Requires a positional argument: the class to which the model is related, which works exactly the same as it does for ForeignKey, including recursive and lazy relationships.'},
+    {'OneToOneField': 'A one-to-one relationship. Conceptually, this is similar to a ForeignKey with unique=True, but the “reverse” side of the relation will directly return a single object.'},
+]
 
-    def __init__(self):
+class ModelMeta(type):
+    def __new__(cls, *args, **kwargs):
+        migration_folder = os.getcwd() +'/migrations/'
+        name = args[0]
+        table_name = args[2]['table_name'] or name
+        sql_query = {
+            table_name: {
 
-        global json_file_path
-        table_name = ''
-        json_file_path = PATH + '/' + f'migrations/{self.table_name}.json'
-        self.json_file_path = json_file_path
-        self.DB_path = PATH + '/' + 'db.sqlite3'
-
-        # getting instance name
-        if True:
-            (filename, line_number, function_name, text) = traceback.extract_stack()[-2]
-            def_name = text[:text.find('=')].strip()
-        self.__table_name__ = def_name
-
-        self.name_in_db = self.__table_name__
-
-    def __name__(self):
-        return self.__table_name__
-
-    def make_migrations(self):
-        table_json = {
-            f'{self.table_name}':
-                {
-                    'Fields': []
-                }
+            }
         }
-        if os.path.isdir(PATH+'/migrations/'):
+        attrs = dict(args[2])
+        for key, value in attrs.items():
+            if key.startswith('__') or key == 'table_name':
+                pass
+            else:
+                sql_query[table_name].update({key: value.create_query().strip()})
+
+
+        if os.path.isdir(migration_folder):
             pass
         else:
-            os.mkdir(PATH+'/migrations/')
-        with open(json_file_path, 'w+') as json_file:
-            json.dump(table_json, json_file)
+            os.mkdir(migration_folder)
 
-    def migrate(self):
-        pass
-        with open(self.json_file_path, 'r') as json_file:
-            fields = json.load(json_file)
-            fields = ', '.join(fields[f'{self.table_name}']['Fields'])
+        with open(migration_folder + name + '.json', 'w+') as json_file:
+            json.dump(sql_query, json_file)
 
-            sql_query = "CREATE TABLE IF NOT EXISTS "\
-                        + self.table_name + "(" \
-                        + fields + ")"
-            with sqlite3.connect(self.DB_path) as connection:
-                cursor = connection.cursor()
-                cursor.execute(sql_query)
-                connection.commit()
-                print('done')
+        return type(*args, **kwargs)
 
-class Field:
-    def __init__(self, table, unique=False, null=False, default=None):
-        self.table = table
+class Field(object):
+    def __init__(self, unique=False, null=False, default=None):
 
         if unique:
             self.unique = "UNIQUE"
@@ -77,75 +68,29 @@ class Field:
             def_name = text[:text.find('=')].strip()
         self.__column_name__ = def_name
 
-    def make_migrations(self):
-        self.query = f"""'{self.__column_name__}' {self.type} {self.null} {self.default} {self.unique}"""
-
-        with open(json_file_path, 'r') as json_file:
-            table = json.load(json_file)
-            table[f'{self.table}']['Fields'] += [self.query]
-            with open(json_file_path, 'w') as json_file:
-                json.dump(table, json_file)
+    def create_query(self):
+        return f""" {self.type} {self.null} {self.default} {self.unique}"""
 
 
     def __name__(self):
         return self.__column_name__
 
 
-
 class CharField(Field):
-    def __init__(self, table, unique=False, null=False, default=None):
-        """
+    """"
+    CharField is A field to store text based values.
+    """
 
-        :rtype: object
-        """
-        self.type = 'TEXT'
-        super().__init__(table, unique, null, default)
+    def __init__(self, max_length, unique=False, null=False, default=None):
 
+        self.max_length = max_length
+        self.type = f'VarChar({max_length})'
+        super().__init__(unique, null, default)
 
-        # getting Field name
-        if True:
-            (filename, line_number, function_name, text) = traceback.extract_stack()[-2]
-            def_name = text[:text.find('=')].strip()
-        self.__column_name__ = def_name
-
-class IntegerField(Field):
-    def __init__(self, table, unique=False, null=False, default=None):
-        self.type = 'INTEGER'
-        super().__init__(table, unique, null, default)
+    def create_query(self):
+        return f""" {self.type} {self.null} {self.default} {self.unique}"""
 
 
-        # getting Field name
-        if True:
-            (filename, line_number, function_name, text) = traceback.extract_stack()[-2]
-            def_name = text[:text.find('=')].strip()
-        self.__column_name__ = def_name
-
-class ImageField(Field):
-    def __init__(self, table, upload_to, unique=False, null=False, default=None):
-        self.type = 'TEXT'
-        super().__init__(table, unique, null, default)
-
-        self.upload_to = upload_to
-
-        if True:
-            (filename, line_number, function_name, text) = traceback.extract_stack()[-2]
-            def_name = text[:text.find('=')].strip()
-        self.__column_name__ = def_name
-
-    def upload(self, image_source):
-        pass
-
-class FileField(Field):
-    def __init__(self, table, upload_to, unique=False, null=False, default=None):
-        self.type = 'TEXT'
-        super().__init__(table, unique, null, default)
-
-        self.upload_to = upload_to
-
-        if True:
-            (filename, line_number, function_name, text) = traceback.extract_stack()[-2]
-            def_name = text[:text.find('=')].strip()
-        self.__column_name__ = def_name
-
-    def upload(self, image_source):
-        pass
+class Test(metaclass=ModelMeta):
+    table_name = 'marwan'
+    field1 = CharField(max_length=10,)
